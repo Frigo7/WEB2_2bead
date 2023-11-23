b3db21db430395362d7f3770a6d999c5431091af
Frigo7 <127610545+Frigo7@users.noreply.github.com>
2023-11-23 21:06:17 +0100

    Add files via upload
    views

---
+++ views/elerhetoseg_alapinfok_main.php
@@ -0,0 +1,40 @@
+<div class="page-heading about-heading header-text" style="background-image: url(assets/images/heading-2-1920x500.jpg);">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="text-content">
+              <h2>SZÖVEG</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+	<div class="best-features">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="section-heading">
+              <h2>ELÉRHETŐSÉG ALMENÜ </h2>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="left-content">
+              <p>Ez az oldal jelenleg fejlesztés alatt áll!
+              <br><br>
+              Kérjük látogasson vissza később<br><br>
+              <br>
+                <h2>Baráti üdvözlettel<h2>
+                <h2>Frank Travel Agency csapata<h2>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="right-image">
+              <img src="assets/images/about.jpg" alt="">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+	
